48bdc1e63dda8650726f0641d7d285517bcafc30
root <root@Becks-PC.localdomain>
2026-07-02 22:03:02 +0200

    a function that prints the elements of a list

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    my_list = [43, 84, 2, 74]
+    my_list = [1, 2, 3]
     for i in my_list:
         print("{:d}".format(i))
 print_list_integer(my_list=[])
