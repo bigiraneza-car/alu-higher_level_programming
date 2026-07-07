53cca391ee12bcb7a5f5acde8423c485c019f34d
root <root@Becks-PC.localdomain>
2026-07-07 10:36:20 +0200

    a function that retrieves an element from a list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list,idx):
     if idx >= len(my_list) or idx < 0:
         return None
     else:
-        print(" Element at index {:d} is {}".format(idx, my_list[idx]))
+        return my_list[idx]
 
 
 if __name__ == "__main__":
