568ddd256bb19526644b9ebd172080fe14b12c81
root <root@Becks-PC.localdomain>
2026-07-10 16:07:37 +0200

    A function that print x elements of a list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,11 +2,10 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     for i in range(x):
-    try:
+        try:
             print(my_list[i], end="")
             count += 1
-    except IndexError:
-        print("Ooops! There are no other element in the list")
-        break
+        except IndexError:
+            break
     print()
     return count
