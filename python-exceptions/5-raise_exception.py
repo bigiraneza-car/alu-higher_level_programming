2f831feed746b8a0ff8ce8d5f9b4e50a44837b2f
root <root@Becks-PC.localdomain>
2026-07-10 17:17:36 +0200

    A function that raises an exception

--- python-exceptions/5-raise_exception.py
+++ python-exceptions/5-raise_exception.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def raise_exception():
-    try:
-        number = input("Enter a number:")
-        if number is not int:
-            raise TypeError
-    except TypeError:
-        print("Exception raised")
+    raise TypeError
