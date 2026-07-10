8b9523f7813a4d8d42a518ec95fb884aa8c165c9
root <root@Becks-PC.localdomain>
2026-07-10 16:44:29 +0200

    A function that divides 2 integers and print the result

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-        print("Inside result:")
+        print("Inside the result: ", end="")
         return result
     except:
         return None
