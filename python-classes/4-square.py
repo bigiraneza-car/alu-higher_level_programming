295466f3c53b0f605d3c521f68251cb101520b0c
bigiraneza-car <b.igiraneza@alustudent.com>
2026-07-20 07:27:40 -0700

    add a getter and a setter in the class

---
+++ python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Defines an empty class square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Reinitialize the private instance attribute size."""
+        self.__size = size
+
+    def size(self):
+        """A getter that return the size value.""" 
+        return self.__size
+
+    def size(self, value):
+        """A setter, that set the final value of the size."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the value of the square area."""
+        return self.__size ** 2
