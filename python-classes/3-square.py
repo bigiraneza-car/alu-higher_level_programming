89a24c7f9160749402bbe77f90f8615902e6c66b
bigiraneza-car <b.igiraneza@alustudent.com>
2026-07-20 07:11:55 -0700

    added a function that return the square of the area

---
+++ python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines an empty class square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Reinitialize the private instance attribute size."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        return size * size
