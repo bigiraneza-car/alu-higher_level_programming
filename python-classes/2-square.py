d3df4bfb56d614a3a5a5fa1a20db7b49adeb2de4
bigiraneza-car <b.igiraneza@alustudent.com>
2026-07-20 07:08:15 -0700

    deleted trailing whitespace for the validation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,9 +7,9 @@ class Square:
 
     def __init__(self, size=0):
         """Reinitialize the private instance attribute size."""
-        if not isinstance(size, int): 
+        if not isinstance(size,int): 
             raise TypeError("size must be an integer")
 
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size =size
+        self.__size = size
