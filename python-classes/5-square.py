a4fd724bb6ee92c7adda9c5c715d2acd511ec5bf
bigiraneza-car <b.igiraneza@alustudent.com>
2026-07-20 07:53:47 -0700

    add a method that draws the square using #

---
+++ python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Defines an empty class square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Reinitialize the private instance attribute size."""
+        self.size = size
+
+    @property
+    def size(self):
+        """A getter that return the size value."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """A setter, that set the final value of the size."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the value of the square area."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print the square using the # character."""
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.__size):
+            print("#" * self.size)
+~
