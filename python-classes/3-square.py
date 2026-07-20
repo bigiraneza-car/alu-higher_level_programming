8903a2ee4b6931c33cbd715e46bc286167a0f8dc
bigiraneza-car <b.igiraneza@alustudent.com>
2026-07-20 07:14:43 -0700

    added the documentation for the function area

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -14,4 +14,5 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
     def area(self):
-        return size * size
+        """Return the value of the square area."""
+        return self.__size ** 2
