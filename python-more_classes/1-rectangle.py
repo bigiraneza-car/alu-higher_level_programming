16a9ead3447194b904ba27b5277d133885d39944
bigiraneza-car <b.igiraneza@alustudent.com>
2026-07-29 16:16:21 +0200

    rectangle class continuation

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -11,13 +11,13 @@ class Rectangle:
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("width must be >=0")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
